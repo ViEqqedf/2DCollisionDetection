9ebfc36d66590c8fd2ebdbfbe62cd9c605ae8a3b
叶凌峰 <vieqqedf@gmail.com>
2022-10-27 21:34:55 +0800

    <feature> more shape test

--- Assets/Scripts/Collision/Shape/Circle.cs
+++ Assets/Scripts/Collision/Shape/Circle.cs
@@ -1,13 +1,54 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Physics.Collision.Shape {
     public class Circle : CollisionShape {
+        private float radius;
+
         public Circle(float radius) : base(ShapeType.Circle) {
+            this.radius = radius;
+
+            localVertices = new List<Vector3>();
+            vertices = new List<Vector3>();
+            for (int i = -2; i < 0; i++) {
+                for (int j = 0; j < 2; j++) {
+                    localVertices.Add(CreateCirclePoint(i, j));
+                    vertices.Add(Vector3.zero);
+                }
+            }
+            for (int i = 0; i < 2; i++) {
+                for (int j = 2; j > 0; j--) {
+                    localVertices.Add(CreateCirclePoint(i, j));
+                    vertices.Add(Vector3.zero);
+                }
+            }
+            for (int i = 2; i > 0; i--) {
+                for (int j = 0; j > -2; j--) {
+                    localVertices.Add(CreateCirclePoint(i, j));
+                    vertices.Add(Vector3.zero);
+                }
+            }
+            for (int i = 0; i > -2; i--) {
+                for (int j = -2; j < 0; j++) {
+                    localVertices.Add(CreateCirclePoint(i, j));
+                    vertices.Add(Vector3.zero);
+                }
+            }
+        }
+
+        private Vector3 CreateCirclePoint(int i, int j) {
+            Vector3 squarePoint = new Vector3((i - 2) * 0.25f, 0, (j - 2) * 0.25f);
+            float squareX = squarePoint.x * radius;
+            float squareZ = squarePoint.z * radius;
+
+            return new Vector3(
+                squareX * Mathf.Sqrt(1 - squareZ * squareZ * 0.5f), 0,
+                squareZ * Mathf.Sqrt(1 - squareX * squareX * 0.5f));
         }
 
         protected override void GetBound(out Vector3 lowerBound, out Vector3 upperBound) {
-            lowerBound = Vector3.zero;
-            upperBound = Vector3.zero;
+            lowerBound = new Vector3(-radius, 0, -radius);
+            upperBound = new Vector3(radius, 0, radius);
         }
     }
 }
--- Assets/Scripts/CollisionObjectProxy.cs
+++ Assets/Scripts/CollisionObjectProxy.cs
@@ -1,25 +1,33 @@
 ﻿using System;
 using Physics.Collision;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Physics {
     public class CollisionObjectProxy : MonoBehaviour {
         public CollisionObject target;
         public bool IsInControl = false;
+        public int velocity = 7;
 
         private void Update() {
             transform.position = target.position;
 
             if (IsInControl) {
-                if (Input.GetKey(KeyCode.A)) {
-                    target.Translate(0.02f * Vector3.left);
-                } else if (Input.GetKey(KeyCode.D)) {
-                    target.Translate(0.02f * Vector3.right);
-                } else if (Input.GetKey(KeyCode.W)) {
-                    target.Translate(0.02f * Vector3.forward);
-                } else if (Input.GetKey(KeyCode.S)) {
-                    target.Translate(0.02f * Vector3.back);
+                if (Input.GetKeyDown(KeyCode.A)) {
+                    target.AddVelocity(velocity * Vector3.left);
+                }
+                if (Input.GetKeyDown(KeyCode.D)) {
+                    target.AddVelocity(velocity * Vector3.right);
+                }
+                if (Input.GetKeyDown(KeyCode.W)) {
+                    target.AddVelocity(velocity * Vector3.forward);
+                }
+                if (Input.GetKeyDown(KeyCode.S)) {
+                    target.AddVelocity(velocity * Vector3.back);
+                }
+
+                if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) ||
+                    Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)) {
+                    target.SetVelocity(Vector3.zero);
                 }
             }
         }
