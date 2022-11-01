69fc75d995cbc98672bc09493e35fcc051857d40
叶凌峰 <yelingfeng@xmfunny.com>
2022-11-01 11:31:16 +0800

    <misc> simulate more player

--- Assets/Scripts/CollisionObjectProxy.cs
+++ Assets/Scripts/CollisionObjectProxy.cs
@@ -7,30 +7,51 @@ namespace Physics {
     public class CollisionObjectProxy : MonoBehaviour {
         public CollisionObject target;
         public ShapeType shape;
-        public bool IsInControl = false;
+        public bool isInControl = false;
+        public int nP = 1;
         public int velocity = 7;
 
         private void Update() {
             transform.position = target.position;
             shape = target.shape.shapeType;
 
-            if (IsInControl) {
-                if (Input.GetKeyDown(KeyCode.A)) {
-                    target.AddVelocity(velocity * Vector3.left);
-                }
-                if (Input.GetKeyDown(KeyCode.D)) {
-                    target.AddVelocity(velocity * Vector3.right);
-                }
-                if (Input.GetKeyDown(KeyCode.W)) {
-                    target.AddVelocity(velocity * Vector3.forward);
-                }
-                if (Input.GetKeyDown(KeyCode.S)) {
-                    target.AddVelocity(velocity * Vector3.back);
-                }
+            if (isInControl) {
+                if (nP == 1) {
+                    if (Input.GetKeyDown(KeyCode.A)) {
+                        target.AddVelocity(velocity * Vector3.left);
+                    }
+                    if (Input.GetKeyDown(KeyCode.D)) {
+                        target.AddVelocity(velocity * Vector3.right);
+                    }
+                    if (Input.GetKeyDown(KeyCode.W)) {
+                        target.AddVelocity(velocity * Vector3.forward);
+                    }
+                    if (Input.GetKeyDown(KeyCode.S)) {
+                        target.AddVelocity(velocity * Vector3.back);
+                    }
 
-                if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) ||
-                    Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)) {
-                    target.SetVelocity(Vector3.zero);
+                    if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) ||
+                        Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)) {
+                        target.SetVelocity(Vector3.zero);
+                    }
+                } else if (nP == 2) {
+                    if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                        target.AddVelocity(velocity * Vector3.left);
+                    }
+                    if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                        target.AddVelocity(velocity * Vector3.right);
+                    }
+                    if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                        target.AddVelocity(velocity * Vector3.forward);
+                    }
+                    if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                        target.AddVelocity(velocity * Vector3.back);
+                    }
+
+                    if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow) ||
+                        Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.RightArrow)) {
+                        target.SetVelocity(Vector3.zero);
+                    }
                 }
             }
         }
